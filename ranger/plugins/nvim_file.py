75adedf5fd1521c832b2a0a614d5e5c21d88ded5
ztoiax <liang1124486160@gmail.com>
2021-04-01 23:56:42 +0800

    add ranger plugin nvim_file.py

---
+++ ranger/plugins/nvim_file.py
@@ -0,0 +1,31 @@
+from ranger.api.commands import Command
+from ranger.container.file import File
+
+
+class nvim_file(Command):
+    '''
+    在已经启动的nvim打开文件,而不是新开一个nvim
+    '''
+    def execute(self):
+        if self.arg(1):
+            filename = self.rest(1)
+        else:
+            filename = str(self.fm.thisfile)
+
+        from pynvim import attach
+
+        try:
+            nvim = attach('socket', path='/tmp/nvim')
+        except :
+            self.fm.notify('connect nvim error')
+            return
+
+        from pathlib import Path
+        p = Path(filename)
+        if p.is_file():
+            nvim.vars['filename'] = filename
+            nvim.command('exe "edit " . g:filename')
+            self.fm.notify(filename)
+        else:
+            self.fm.notify(filename + ' is not file')
+        return
